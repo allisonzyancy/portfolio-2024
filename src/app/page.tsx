d6a9881bd48d137da14f9b3679a9e087ece0e436
Allison Yancy <yancyshmancy@gmail.com>
2024-08-05 16:50:02 -0400

    Changed text color on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -37,8 +37,8 @@ export default async function Index() {
     <div className="lg:flex lg:justify-between lg:gap-4">
       <header className="lg:sticky lg:top-0 lg:flex lg:max-h-screen lg:w-1/2 lg:flex-col lg:justify-between lg:py-24">
         <div className="sm:pr-24">
-          <h1 className="text-4xl font-bold tracking-tight text-pink-400 sm:text-5xl">{home.data.title}</h1>
-          <h2 className="mt-3 text-lg font-medium tracking-tight text-slate-200 sm:text-xl mb-4">{home.data.subtitle}</h2>
+          <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">{home.data.title}</h1>
+          <h2 className="mt-3 text-lg font-medium tracking-tight text-pink-400 sm:text-xl mb-4">{home.data.subtitle}</h2>
           <PrismicRichText field={home.data.lead_text} components={{ strong: ({ children }) => <span className="font-bold text-pink-300">{children}</span>}}/>
 
           <nav className="nav">
